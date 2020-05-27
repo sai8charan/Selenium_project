e82418fa2d17027e0390c9b2acb71198f0a57c67
swath <swath@LAPTOP-C934F65Q>
2020-05-27 19:45:50 +0530

    commit pom1

--- Coursera_automation/src/main/java/utilites/DriverSetup.java
+++ Coursera_automation/src/main/java/utilites/DriverSetup.java
@@ -1,9 +1,20 @@
 package utilites;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverSetup {
 
-	
-	
+	private static String url = "https://www.coursera.org/";
+	public static WebDriver getWebDriver()
+	{
+		String path = System.getProperty("usr.dir");
+		System.setProperty("webdriver.chrome.driver", path+"\\Drivers\\chromedriver.exe");
+		WebDriver driver =  new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
 	
 	
 	
--- Coursera_automation/src/main/java/webPages/ResultPage.java
+++ Coursera_automation/src/main/java/webPages/ResultPage.java
@@ -1,8 +1,26 @@
 package webPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import utilites.*;
+
 public class ResultPage {
 
+	//WebDriver driver = null;
+	
+
 	//apply language filter
+	
+	public static void applyFilter(WebDriver driver, String lang)
+	{
+		WebElement languageFilter =driver.findElement(By.xpath(Locators.get_LanguageFilter()));
+		Select language = new Select(languageFilter);
+		language. selectByVisibleText(lang);
+		
+	}
 	
 	//apply level filter 
 	

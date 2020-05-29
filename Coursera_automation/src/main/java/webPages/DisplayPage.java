6c40dd649af0ff99b95bdd788fff0a32629f5187
sai8charan <65341947+sai8charan@users.noreply.github.com>
2020-05-29 16:06:07 +0530

    Update DisplayPage.java

--- Coursera_automation/src/main/java/webPages/DisplayPage.java
+++ Coursera_automation/src/main/java/webPages/DisplayPage.java
@@ -3,10 +3,45 @@ package webPages;
 public class DisplayPage {
 
 	// Get course details 
+	public String getCourseName(String course_Name)
+	{
+		String Name = course_Nmae.getText();
+		return Name;
+	}
+	
+	public String getRating(String ratings)
+	{
+		String Rating  = ratings.getText();
+		return Rating;
+	}
+	
+	public String getLevel(String level)
+	{
+		String Course_level = level.getText();
+		return Course_level;
+	}
+	
+	
+	public String getDuration(String duration)
+	{
+		String Course_duration = duration.getText();
+		return Course_duration;
+	}
+	
+	public String getLanguage(String language)
+	{
+		String lang  = language.getText();
+		return lang;
+	}
+	
+	
+		
 	
 	
 	
 	//navigate to back page
+	
+	
 	
 	
 }
